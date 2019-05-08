8b960d8612f0e4c0c4b618b3a876c037ce65d33d
橋本 陽生 <hashimotoharuki@team-lab.com>
2019-05-08 18:29:46 +0900

    volumeコントロール可能に

--- keyboards/lily58/keymaps/haruki/keymap.c
+++ keyboards/lily58/keymaps/haruki/keymap.c
@@ -76,15 +76,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     \
   _______,    KC_7,    KC_8,    KC_9, _______, _______,                     _______, _______, _______, _______, _______, _______, \
-  _______,   KC_4,    KC_5,  KC_6, _______, _______,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD, \
-  KC_GRV,   KC_1,   KC_2,  KC_3, KC_DLR,  KC_PERC, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  _______,   KC_4,    KC_5,  KC_6, _______, _______,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,      \
+  KC_GRV,   KC_1,   KC_2,  KC_3, KC_DLR,  KC_PERC, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,        \
                                KC_0, _______, _______, _______, _______,  _______, _______, _______ \
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |VolDwn |volUp| Mute |      |BriDwn |BriUp|                    |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |                    |VolDwn|volUp | Mute |      |BriDwn|BriUp |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |  UP  |      |      |      |                    | LGUI | RGUI |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT( \
-  KC_VOLD, KC_VOLU, KC_MUTE, _______, KC_BRID, KC_BRIU,                     _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______,                     KC_BRID, KC_BRIU, _______, KC_VOLD, KC_VOLU, KC_MUTE, \
   _______, _______,  KC_UP, _______, _______, _______,                      KC_LGUI, KC_RGUI, _______, _______, _______, _______, \
   _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______,                       KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,   XXXXXXX, XXXXXXX, \
   _______, _______, _______, _______, _______, _______,  _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
